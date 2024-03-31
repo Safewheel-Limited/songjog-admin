43185574c55e27beebbad14add3df16e8541ae26
mdalamin3091 <alamin@safewheel.zyz>
2024-03-31 13:59:08 +0600

    add care package delete

--- src/app/(dashboard)/(carePackages)/package-time/_comonents/package-time-lists.column.tsx
+++ src/app/(dashboard)/(carePackages)/package-time/_comonents/package-time-lists.column.tsx
@@ -1,4 +1,34 @@
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Button, Popconfirm, Space, message } from "antd";
+
+import { DELETE_CARE_PACKAGE_TIME } from "../../graphql";
+import { KeyEnum } from "@/common/constants";
+import { useMutation } from "@apollo/client";
+
 const PackageListsColumnRenderer = () => {
+
+    const [carePackageTimeDelete, { loading }] = useMutation(DELETE_CARE_PACKAGE_TIME, {
+        refetchQueries: ["carePackageTimeGetAll"],
+    });
+
+    const handleDeletePackageTime = (id: string | number) => {
+        carePackageTimeDelete({
+            variables: {
+                input: id
+            }
+        }).then(res => {
+            if (res.data) {
+                message.success("Package Time deleted Successful")
+            }
+        }).catch(err => {
+            message.error(err.message);
+        })
+    }
+
+    const handleUpdatePackageTime = (id: string) => {
+
+    }
+
     const columns = [
         {
             title: "Id",
@@ -33,6 +63,37 @@ const PackageListsColumnRenderer = () => {
                     })}
                 </p>
             ),
+        },
+        {
+            title: "Actions",
+            dataIndex: "actions",
+            key: "actions",
+            // width: 120,
+            render: (_: any, data: any) => {
+                return (
+                    //   <Permission>
+                    <Space>
+                        <Button
+                            key={KeyEnum.ROLE_ACCESS_UPDATE}
+                            type="primary"
+                            icon={<EditOutlined />}
+                            style={{ background: "#4682A9" }}
+                            onClick={() => handleUpdatePackageTime(data.id)}
+                        />
+                        <Popconfirm
+                            title="Are you sure ?"
+                            description="Are you sure to delete this Role"
+                            onConfirm={() => handleDeletePackageTime(data.id)}
+                            okText="Yes"
+                            cancelText="No"
+                            disabled={loading}
+                        >
+                            <Button key={KeyEnum.ROLE_ACCESS_DELETE} icon={<DeleteOutlined />} danger type="primary" />
+                        </Popconfirm>
+                    </Space>
+                    //   </Permission>
+                );
+            },
         },
 
     ];
